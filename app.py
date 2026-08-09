40998d4cee53cda07a05d44101800f1e92976342
Gireesh Kumar Gowd <gireeshkumargowd@gmail.com>
2026-08-09 22:58:54 +0530

    Add exception.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from src.youtubeViewerSentimentAnalysis.logger import logging
+from src.youtubeViewerSentimentAnalysis.exception import CustomException
+import sys
 
-if __name__=="__main__":
-    logging.info("Logging has started")
+
+if __name__ == "__main__":
+    logging.info("The execution has started")
