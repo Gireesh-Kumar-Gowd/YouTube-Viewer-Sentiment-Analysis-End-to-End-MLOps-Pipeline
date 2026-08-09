cb30dfeed2c79e038592dd3b3854f21957b4e1a1
Gireesh Kumar Gowd <gireeshkumargowd@gmail.com>
2026-08-09 17:36:28 +0530

    changed name

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def get_requirements(file_path: str) -> List[str]:
     return requirements
 
 setup(
-    name='End to End Data Science Project',
+    name='YouTube-Viewer-Sentiment-Analysis',
     version='0.0.1',
     author='A.Gireesh kumar Gowd',
     author_email='gireeshkumargowd@gmail.com',
